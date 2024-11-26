9d87283b9c2c6c8c40acfec5cfb3ce802a01a3af
Ramos <andfera201@gmail.com>
2024-11-25 19:39:15 -0500

    refactor: [200~Improvement in variables and their use in login

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,135 @@
-import { Box, Button, Container, IconButton, InputAdornment, TextField, Typography } from "@mui/material"
-import { Visibility, VisibilityOff } from "@mui/icons-material"
-import { useState } from "react"
-
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { useState } from "react";
 
 function App(): JSX.Element {
-
-  const [userFieldForm, setUserFieldForm] = useState('');
-  const [ passwordFieldForm, setPasswordFieldForm] = useState('');
-
-  const [hasErrorUserFieldForm, setHasErrorUserFieldForm] = useState(false);
-  const [hasErrorPasswordFieldForm, setHasErrorPasswordFieldForm] = useState(false);
+  const [formInputFields, setFormInputFields] = useState({
+      UserField: '',
+      PasswordField: '',
+      hasErrorUserField: false,
+      hasErrorPasswordField: false
+  })
 
   const [isShowPassword, setIsShowPassword] = useState(false);
-  const toggleShowPassword = (): void => { setIsShowPassword( (prev) => !prev) }
+
+  const toggleShowPassword = (): void => setIsShowPassword((prev) => !prev)
   
+
   const handleSumit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    
-    setHasErrorUserFieldForm(userFieldForm === '')
-    setHasErrorPasswordFieldForm(passwordFieldForm === '')
 
-    if (userFieldForm && passwordFieldForm) {
-      alert('Success...')
+    setFormInputFields( prevState => ({
+      ...prevState,
+      hasErrorUserField: formInputFields.UserField === '',
+      hasErrorPasswordField: formInputFields.PasswordField === '',
+    })) 
+    
+
+    if (formInputFields.UserField && formInputFields.PasswordField) {
+      alert("Success...");
     }
-  }
+  };
 
   return (
-    <Box component="section" sx={{
-      minHeight: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      bgcolor: "rgb(244, 244, 244)" 
-    }} >
-      <Container sx={{
+    <Box
+      component="section"
+      sx={{
+        minHeight: "100vh",
         display: "flex",
-        flexDirection: "column",
+        alignItems: "center",
         justifyContent: "center",
-        textAlign: "center",
-        boxShadow: 3,
-        borderRadius: 4,
-        p: 3,
-        maxWidth: {sm: 500},
-        bgcolor: "rgb(255, 255, 255)"
-      }} >
-        <Typography variant="h4" >
-          Iniciar Sesion
-        </Typography>
-        <Typography sx={{
-          fontSize: "18px",
-          opacity: 0.6
-        }} >
+        bgcolor: "rgb(244, 244, 244)",
+      }}
+    >
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          textAlign: "center",
+          boxShadow: 3,
+          borderRadius: 4,
+          p: 3,
+          maxWidth: { sm: 500 },
+          bgcolor: "rgb(255, 255, 255)",
+        }}
+      >
+        <Typography variant="h4">Iniciar Sesión</Typography>
+        <Typography
+          sx={{
+            fontSize: "18px",
+            opacity: 0.6,
+          }}
+        >
           Por favor inicia sesión para continuar
         </Typography>
-    
-        <form onSubmit={handleSumit} >
-          <TextField 
+
+        <form onSubmit={handleSumit}>
+          <TextField
             required
             fullWidth
-            type="text" 
-            value={userFieldForm} 
+            type="text"
+            value={formInputFields.UserField}
             label="Usuario"
             variant="standard"
-            sx={{ mt: 3 }}
-            error={hasErrorUserFieldForm}
-            helperText={ hasErrorUserFieldForm ?  'Este campo es requerido.' : ''  }
-            onChange={ (e) => { 
-              setUserFieldForm(e.target.value)
-              setHasErrorUserFieldForm(false)
-            }} 
+            sx={{mt: 3}}
+            error={formInputFields.hasErrorUserField}
+            helperText={ formInputFields.hasErrorUserField ? 'Este campo es requerido.' : '' }
+            onChange={ (e) => {
+              setFormInputFields( prevState => ({
+                ...prevState,
+                UserField: e.target.value,
+                hasErrorUserField: false,
+              }))
+            }}
           />
 
-          <TextField 
+          <TextField
             required
             fullWidth
-            type={ isShowPassword ? 'text' : 'password' }
-            value={passwordFieldForm} 
+            type={isShowPassword ? 'text' : 'password'}
+            value={formInputFields.PasswordField}
             label="Contraseña"
             variant="standard"
-            sx={{ mt: 3 }}
-            error={hasErrorPasswordFieldForm}
-            helperText={ hasErrorPasswordFieldForm ?  'Este campo es requerido.' : ''  }
+            sx={{mt: 3}}
+            error={formInputFields.hasErrorPasswordField}
+            helperText={ formInputFields.hasErrorPasswordField ? 'Este campo es requerido.' : '' }
             onChange={ (e) => {
-              setPasswordFieldForm(e.target.value)
-              setHasErrorPasswordFieldForm(false)
+              setFormInputFields( prevState => ({
+                ...prevState,
+                PasswordField: e.target.value,
+                hasErrorPasswordField: false,
+              }))
             }}
             slotProps={{
-              input:{ 
+              input: {
                 endAdornment: (
-                  <InputAdornment position='end' >
-                    <IconButton onClick={toggleShowPassword} onMouseDown={ (e) => e.preventDefault() } >
-                      { isShowPassword ? <VisibilityOff /> : <Visibility /> }
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={toggleShowPassword}
+                      onMouseDown={(e) => e.preventDefault()}
+                    >
+                      {isShowPassword ? <Visibility /> : <VisibilityOff />}
                     </IconButton>
                   </InputAdornment>
-              )}
+                ),
+              },
             }}
           />
-          
-          <Button 
+
+          <Button
             fullWidth
-            type="submit" 
-            variant="contained" 
-            color="primary" 
+            type="submit"
+            variant="contained"
+            color="primary"
             size="large"
             sx={{ mt: 3 }}
           >
@@ -109,8 +137,8 @@ function App(): JSX.Element {
           </Button>
         </form>
       </Container>
-    </Box>  
-  )
+    </Box>
+  );
 }
 
-export default App
+export default App;
